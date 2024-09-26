aa004bf61b7441765d8c98e1bd02bb7ab8932e47
Alexandria P. <alxpettit@gmail.com>
2024-09-26 17:18:30 -0600

    template

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 # FIXME: `cargo run` should automatically flash pico
-
 {
   description = "Build a cargo project without extra checks";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.11";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -18,20 +18,30 @@
       url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    flake-utils,
+    rust-overlay,
+    ...
+  } @ inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      perSystem = {
+        system,
+        pkgs,
+        ...
+      }: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ (import rust-overlay) ];
+          overlays = [(import rust-overlay)];
         };
         rust = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-analyzer" "rust-src" ];
-          targets = [ "x86_64-unknown-linux-gnu" "thumbv6m-none-eabi" ];
+          extensions = ["rust-analyzer" "rust-src"];
+          targets = ["x86_64-unknown-linux-gnu" "thumbv6m-none-eabi"];
         };
 
         commonBuildInputs = with pkgs; [
@@ -41,7 +51,7 @@
           elf2uf2-rs
         ];
         craneLib = (crane.mkLib pkgs).overrideToolchain rust;
-                
+
         my-crate = craneLib.buildPackage {
           src = ./.; #craneLib.cleanCargoSource (craneLib.path ./.);
           strictDeps = true;
@@ -49,12 +59,14 @@
           nativeBuildInputs = commonBuildInputs;
           # Breaks on cross compile for RP2040
           doCheck = false;
-          buildInputs = [
-            # Add additional build inputs here
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            # Additional darwin specific inputs can be set here
-            pkgs.libiconv
-          ];
+          buildInputs =
+            [
+              # Add additional build inputs here
+            ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+              # Additional darwin specific inputs can be set here
+              pkgs.libiconv
+            ];
 
           extraDummyScript = ''
             cp -a ${./memory.x} $out/memory.x
@@ -63,8 +75,7 @@
           # Additional environment variables can be set directly
           # MY_CUSTOM_VAR = "some value";
         };
-      in
-      {
+      in {
         checks = {
           inherit my-crate;
         };
@@ -74,19 +85,5 @@
         apps.default = flake-utils.lib.mkApp {
           drv = my-crate;
         };
-
-        devShells.default = craneLib.devShell {
-          # Inherit inputs from checks.
-          checks = self.checks.${system};
-          nativeBuildInputs = commonBuildInputs;
-
-          # Additional dev-shell environment variables can be set directly
-          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
-
-          # Extra inputs can be added here; cargo and rustc are provided by default.
-          packages = [
-            # pkgs.ripgrep
-          ];
-        };
-      });
-}
+      };
+    };
